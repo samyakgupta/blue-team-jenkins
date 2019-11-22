907552047f7f33a5b0f2a502a48945a1173ffe38
mukesh.adelkar <mukesh.adelkar@mukeshalkarsmbp.clients.asia-pac.shell.com>
2019-11-22 10:47:38 +0530

    made changes to main.tf

--- main.tf
+++ main.tf
@@ -9,17 +9,12 @@ terraform {
 }
 
 
-<<<<<<< HEAD
 variable "master_ami" {
   default = "ami-0ca4ee4403ecb3a4e"
 }
 
 variable "node_ami" {
   default = "ami-0ca4ee4403ecb3a4e"
-=======
-variable "jenkins_ami" {
-  default = "ami-asdfasdf"
->>>>>>> 6a80dc77e2ad17625b42a59b0e7086f14557f8ee
 }
 
 provider "aws" {
@@ -32,13 +27,10 @@ module "jenkins_master_module" {
   ami = "${var.jenkins_ami}"
 }
 
-<<<<<<< HEAD
 module "jenkins_node_module" {
    source = "./modules/node"
    ami = "${var.node_ami}"
  }
 
-=======
->>>>>>> 6a80dc77e2ad17625b42a59b0e7086f14557f8ee
 
 
